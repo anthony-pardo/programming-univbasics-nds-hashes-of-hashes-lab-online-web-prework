cbfd171c6ac77156cafeb9e068afadc35580ff67
Anthony Pardo <pardo.anthony@gmail.com>
2019-10-28 22:05:59 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -68,6 +68,8 @@ def naming_system
   
   hoh = PORTION_1
   hoh[:sub_category][:sub_category][:sub_category] = PORTION_2
+  hoh[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+
   hoh
 
 end
